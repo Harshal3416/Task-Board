6ccce4e0adfd877e35bb2b5150fc22c58e556c5f
Harshal3416 <harshalbelamkar5@gmail.com>
2020-06-01 12:45:49 +0530

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'taskBoard';
+export class AppComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
+
+  newTask = { name: '', desc: '', date: new Date() };
+
+  todo = [];
+  doing = [];
+  done = [];
+  rejected = [];
+
+  ngOnInit() {
+    let localtodo = localStorage.getItem('todo');
+    if (localtodo !== null) {
+      this.todo = JSON.parse(localtodo);
+    }
+
+    let localdoing = localStorage.getItem('doing');
+    if (localdoing !== null) {
+      this.doing = JSON.parse(localdoing);
+    }
+
+    let localdone = localStorage.getItem('done');
+    if (localdone !== null) {
+      this.done = JSON.parse(localdone);
+    }
+
+    let localrejected = localStorage.getItem('rejected');
+    if (localrejected !== null) {
+      this.rejected = JSON.parse(localrejected);
+    }
+  }
+
+  addtask() {
+    this.newTask.date = new Date();
+    this.todo.push(this.newTask);
+    this.newTask = { name: null, desc: null, date: null };
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+  }
+
+  drop(event: CdkDragDrop<any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+    localStorage.setItem('doing', JSON.stringify(this.doing));
+    localStorage.setItem('done', JSON.stringify(this.done));
+    localStorage.setItem('rejected', JSON.stringify(this.rejected));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
